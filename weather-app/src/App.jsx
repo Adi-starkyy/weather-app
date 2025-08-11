2483393017dafc818c180909005334a0f13576d5
Adrijo Bhowmik <98826098+Adi-starkyy@users.noreply.github.com>
2025-08-11 19:26:15 +0530

    update

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -23,8 +23,8 @@ const App = () => {
   console.log(weather);
 
   return (
-    <div className="app h-80 w-100 bg-gradient-to-br from-amber-200 to-yellow-100 shadow-lg p-6 flex flex-col items-center justify-between space-y-4">
-      <h1 className="text-3xl font-bold text-gray-800">🌤 Weather App</h1>
+    <div className="app h-90 w-100 bg-gradient-to-br from-amber-200 to-yellow-100 shadow-lg p-6 flex flex-col items-center justify-between space-y-4">
+      <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-amber-500 to-yellow-500 text-center">🌤 Weather App</h1>
 
       <div className="w-full flex flex-col items-center space-y-3">
         <Input className="w-4/5 px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-400" />
@@ -33,16 +33,11 @@ const App = () => {
             onClick={searchButton} 
             value = "Search"
           />
-          <Button 
-            className=""
-            value = " Refresh"
-          />
         </div>
       </div>
 
       <Card className="w-full bg-white rounded-xl shadow-inner p-4" />
-  </div>
-
+   </div>
   )
 }
 
--- weather-app/src/components/input.jsx
+++ weather-app/src/components/input.jsx
@@ -9,7 +9,7 @@ const Input = () => {
       placeholder="Search"
       value={weather.searchCity}
       onChange={(e) => weather.setSearchCity(e.target.value)}
-      className="w-4/5 px-4 py-2 rounded-xl border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-400 transition-all"
+      className="px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-400 ${className}"
     />
   )
 }
